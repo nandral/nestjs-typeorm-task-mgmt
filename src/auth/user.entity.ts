e9715e67f0c6268a404c578cd51f22284acd0ce0
Niranjan <niranjan@mbp.home>
2020-06-11 14:30:07 +1000

    Update config

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -7,7 +7,7 @@ import {
   OneToMany,
 } from 'typeorm';
 
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Task } from 'src/tasks/task.entity';
 
 @Entity()
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,23 @@ import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
-  const port = process.env.PORT || serverConfig.port;
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    //TODO enable below line after origin is setup
+    // app.enableCors({ origin: serverConfig.origin });
+    app.enableCors();
+    // logger.log(`Accepting requests from origin ${serverConfig.origin}`);
+    logger.log('Accepting requests .. PROD ###');
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+
   await app.listen(port);
+
   logger.log(`Application listening on ${port}`);
 }
 bootstrap();
